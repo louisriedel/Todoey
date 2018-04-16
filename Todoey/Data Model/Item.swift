d85fb97d270b17943afa466d985d44e9ebb5afb3
Louis Riedel <riedel.louis@icloud.com>
2018-04-16 20:36:56 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,23 @@
+//
+//  DataModel.swift
+//  Todoey
+//
+//  Created by Louis Riedel on 16.04.18.
+//  Copyright © 2018 Louis Riedel. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+    
+    
+    
+    
+    
+    
+    
+}
